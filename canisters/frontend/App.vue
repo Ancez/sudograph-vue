c6a81ede03f3d4b71c0eae67e7d84222352a20d9
ancez <Lukasz.czapiewski@mmtm.io>
2021-06-23 23:41:22 +0100

    initial implementation of vue with sudograph; moved away from snowpack as not familiar

---
+++ canisters/frontend/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>sudograph-vue</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: () => {
+    return {
+      internetComputerGreeting: ''
+    }
+  },
+  created() {
+  }
+}
+</script>
